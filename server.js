b7637388daeb4cb4925e9bd79eb56717e1917b79
xamidullo <khkhamidullo@gmail.com>
2025-01-11 14:53:14 +0500

    update constroller

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use('/test', (req, res) => {
 app.use('/test2', (req, res) => {
   res.send("test 1 completed")
 })
+app.use('/abc', (req, res) => {
+  res.send("abc!")
+})
 
 app.listen(1956, () => {
   console.log("server  on va  in too 1956 port");
