ae6a25a84a80d157ed7508b6fb921005f7c0a5bb
xamidullo <khkhamidullo@gmail.com>
2025-01-14 11:06:42 +0500

    update

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use('/test', (req, res) => {
-  res.send("test 1 completed")
-})
+  res.send('test 1 completed');
+});
 
 app.use('/test2', (req, res) => {
-  res.send("test 1 completed")
-})
+  res.send('test 1 completed');
+});
+
 app.use('/abc', (req, res) => {
-  res.send("abc!")
-})
+  res.send('abc!');
+});
+
 app.use('/aaa', (req, res) => {
-  res.send("aaa!")
-})
+  res.send('aaa!');
+});
+
+app.use('/najot', (req, res) => {
+  res.send('talim!');
+});
 
 app.listen(1966, () => {
-  console.log("server  on va  in too 1966 port");
+  console.log('server  on va  in too 1966 port');
 });
